0d4c8322ca56f12add149d8716ae76060fee2dc6
louzhiyuan <34015181+louzhiyuan@users.noreply.github.com>
2018-04-08 13:42:23 +0800

    第一步

--- src/main/java/com/demo/rsa/RSAUtils/RSAKeyCreate.java
+++ src/main/java/com/demo/rsa/RSAUtils/RSAKeyCreate.java
@@ -37,4 +37,21 @@ public class RSAKeyCreate {
         createKeyPairs();
     }
 
+    /*
+    *公钥Base64编码:
+    *MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAOCeyL9fEkpjDSWLUu9y
+    *AfpMPzM6x+q+YLPyRvhNZe+qTqWLQvDeLO47VXzzBiqsKTjl90kZ
+    *MWkykBF9whK5OckCAwEAAQ==
+    *
+    *私钥Base64编码
+    *MIIBVgIBADANBgkqhkiG9w0BAQEFAASCAUAwggE8AgEAAkEA4J7Iv1
+    *8SSmMNJYtS73IB+kw/MzrH6r5gs/JG+E1l76pOpYtC8N4s7jtVfPMG
+    *KqwpOOX3SRkxaTKQEX3CErk5yQIDAQABAkEAt0hZPe5xmkkgnRYGYm
+    *OAwaAdocvA/QbDGZID3vAo0Vl6wfgX1orhfeK8IbzC47Zn4WanZzLp
+    *N5D756zGRVC9QQIhAPYmfHJAMTpFPHVuVcCuuhqHtT8D47KiWElMxD
+    *j5JFDVAiEA6Zu/jLzXV9au+hP4wuMh0P7nL0VolQjxlUGSTz3Q3yUC
+    *IQCeg5JIkQYinEue4/rdJqg6RYJ2ni6X7uwj6AfFGMxGMQIgHSIEQF
+    *voixbF2YAWHM/QjszLLfT89uEXp6J2yZUO8FUCIQCvaslJCd6lnief
+    *P5yTu05fXfPGCD5/G4iC2ROapdDqgA==
+    */
 }
